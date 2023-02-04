01bd15c20c7cd0b885a6295927e0aa03eaeb1ed2
Jubayer Juhan <davidjuhan23@gmail.com>
2023-02-05 02:04:23 +0600

    Hero Page Text added

--- src/components/ui/Herosection/Herosection.jsx
+++ src/components/ui/Herosection/Herosection.jsx
@@ -13,6 +13,13 @@ const Herosection = () => {
       <div className={styles.tagline}>
         <h3>I build things for the web.</h3>
       </div>
+      <div className={styles.description}>
+        <p>
+          I’m a software engineer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I’m focused on
+          building accessible, human-centered products at Upstatement.
+        </p>
+      </div>
     </section>
   );
 };
