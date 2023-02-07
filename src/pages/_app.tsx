48507fe330049edd314e280f5b6a0ad5fc492ed4
Jubayer Juhan <davidjuhan23@gmail.com>
2023-02-07 21:52:08 +0600

    Font loaded with next font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import localFont from "@next/font/local";
+import { useEffect, useState } from "react";
+
+const font = localFont({
+  src: [
+    {
+      path: "../font/test-calibre-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../font/test-calibre-semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={font.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
