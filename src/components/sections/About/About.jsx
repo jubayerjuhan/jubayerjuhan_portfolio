b0eaf5bac9a149c21e795ab67b15dc368a50d037
Jubayer Juhan <davidjuhan23@gmail.com>
2023-02-07 16:42:39 +0600

    About section some extra padding added 🪄

--- src/components/sections/About/About.jsx
+++ src/components/sections/About/About.jsx
@@ -7,54 +7,59 @@ import animation from "../../../styles/Animation/slide_animation.module.scss";
 import styles from "./about.module.scss";
 
 const About = () => {
-  const { ref, inView, entry } = useInView({ threshold: 0 });
+  const { ref, inView } = useInView({ threshold: 0 });
 
   return (
-    <div
-      className={` sectionPadding ${styles.about} ${
-        inView && styles.showAbout
-      }`}
-    >
-      <SectionHeader />
-      <div className={styles.contentWrapper}>
-        <div className={styles.textSection} ref={ref}>
-          <div className={styles.description}>
-            <p>
-              Hello! My name is Brittany and I enjoy creating things that live
-              on the internet. My interest in web development started back in
-              2012 when I decided to try editing custom Tumblr themes — turns
-              out hacking together a custom reblog button taught me a lot about
-              HTML & CSS!{" "}
-            </p>
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at an
-              advertising agency, a start-up, a huge corporation, and a
-              student-led design studio. My main focus these days is building
-              accessible, inclusive products and digital experiences at
-              Upstatement for a variety of clients.
-            </p>
-            <p>
-              I also recently launched a course that covers everything you need
-              to build a web app with the Spotify API using Node & React.
-            </p>
-            <p> Here are a few technologies I’ve been working with recently:</p>
+    <section className={styles.aboutWrapper}>
+      <div
+        className={` sectionPadding ${styles.about} ${
+          inView && styles.showAbout
+        }`}
+      >
+        <SectionHeader />
+        <div className={styles.contentWrapper}>
+          <div className={styles.textSection} ref={ref}>
+            <div className={styles.description}>
+              <p>
+                Hello! My name is Brittany and I enjoy creating things that live
+                on the internet. My interest in web development started back in
+                2012 when I decided to try editing custom Tumblr themes — turns
+                out hacking together a custom reblog button taught me a lot
+                about HTML & CSS!{" "}
+              </p>
+              <p>
+                Fast-forward to today, and I’ve had the privilege of working at
+                an advertising agency, a start-up, a huge corporation, and a
+                student-led design studio. My main focus these days is building
+                accessible, inclusive products and digital experiences at
+                Upstatement for a variety of clients.
+              </p>
+              <p>
+                I also recently launched a course that covers everything you
+                need to build a web app with the Spotify API using Node & React.
+              </p>
+              <p>
+                {" "}
+                Here are a few technologies I’ve been working with recently:
+              </p>
+            </div>
+            <div className={styles.skills}>
+              <p className={styles.skill}>Javascript</p>
+              <p className={styles.skill}>React</p>
+              <p className={styles.skill}>Node.js</p>
+              <p className={styles.skill}>Express</p>
+              <p className={styles.skill}>TypeScript</p>
+              <p className={styles.skill}>Redux</p>
+            </div>
           </div>
-          <div className={styles.skills}>
-            <p className={styles.skill}>Javascript</p>
-            <p className={styles.skill}>React</p>
-            <p className={styles.skill}>Node.js</p>
-            <p className={styles.skill}>Express</p>
-            <p className={styles.skill}>TypeScript</p>
-            <p className={styles.skill}>Redux</p>
-          </div>
-        </div>
-        <div className={styles.mediaSection}>
-          <div className={styles.profilePicture__wrapper}>
-            <Image src={profilePicture} alt="Jubayer Juhan Profile Picture" />
+          <div className={styles.mediaSection}>
+            <div className={styles.profilePicture__wrapper}>
+              <Image src={profilePicture} alt="Jubayer Juhan Profile Picture" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
