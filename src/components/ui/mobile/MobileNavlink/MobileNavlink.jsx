909bd675193c49828e065bc8c62c38388de1781c
Jubayer Juhan <davidjuhan23@gmail.com>
2023-02-12 21:34:29 +0600

    Navbar(Mobile) Navlink Opacity Fixed

--- src/components/ui/mobile/MobileNavlink/MobileNavlink.jsx
+++ src/components/ui/mobile/MobileNavlink/MobileNavlink.jsx
@@ -4,7 +4,7 @@ import navStyle from "./mobileNavlink.module.scss";
 
 const MobileNavlink = ({ navlink, styles, index: key }) => {
   const { ref, inView, entry } = useInView({ threshold: 0 });
-  console.log(inView, "inview..");
+  console.log(inView, "inview..yooo");
 
   return (
     <div
